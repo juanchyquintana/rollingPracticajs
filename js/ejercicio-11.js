0aae258553d1ee228d0f984b757436871263aeb1
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-10-25 15:01:10 -0300

    Se realizó el ejercicio once

---
+++ js/ejercicio-11.js
@@ -0,0 +1,8 @@
+// Ejercicio 11
+
+const num = parseInt(prompt('Introduzca un número, por favor.'));
+if(num % 2 === 0) {
+    document.write(`El ${num} es divisible por 2`);
+} else {
+    document.write(`El ${num} NO es divisible por 2`);
+}
