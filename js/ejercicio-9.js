cad94ed348719b5eaef3e44da193eeec60fbc8ce
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-10-25 14:43:36 -0300

    se realizó el noveno ejercicio

---
+++ js/ejercicio-9.js
@@ -0,0 +1,9 @@
+// Ejercicio 9
+let num1 = parseInt(prompt('Introduce un número, por favor.'))
+let num2 = parseInt(prompt('Introduce otro número, por favor.'))
+
+if(num1 > num2) {
+    document.write(`El Número ${num1} es mayor`);
+} else {
+    document.write(`El Número ${num2} es mayor`);
+}
