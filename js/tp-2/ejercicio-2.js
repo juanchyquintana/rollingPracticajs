3bea6af5f51c21af1a8d6a06aa07236f7846dff1
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-10-31 13:41:44 -0300

    se realizó el ejercicio del banco

---
+++ js/tp-2/ejercicio-2.js
@@ -0,0 +1,39 @@
+const dinero = parseInt(prompt("Ingrese su saldo disponible"))
+const opcion = parseInt(prompt('Seleccione una opción: 1- consultar el saldo, 2- extraer dinero, 3- ingresar dinero'));
+let saldo;
+
+// if(opcion === 1){
+//     document.write(`Su saldo disponible es ${dinero}`);
+// }else if(opcion === 2){
+//     const extraer = parseInt(prompt("¿Cuanto quiere extraer?"))
+//     saldo = dinero - extraer
+//     document.write(`Usted extrajo ${extraer}. Su saldo actual es ${saldo}`);
+// }else if(opcion === 3){
+//     const ingreso = parseInt(prompt("¿Cuanto quiere ingresar?"))
+//     saldo = dinero + ingreso
+//     document.write(`Usted ingresó ${ingreso}. Su saldo actual es ${saldo}`);
+// }else{
+//     document.write('ingreso una opción invalida');
+// }
+
+switch (opcion) {
+    case 1:
+        document.write(`Su saldo disponible es ${dinero}`);
+        break;
+    case 2:
+        const extraer = parseInt(prompt("¿Cuanto quiere extraer?"))
+        saldo = dinero - extraer
+        document.write(`Usted extrajo ${extraer}. Su saldo actual es ${saldo}`);
+        break;
+    case 3:
+        const ingreso = parseInt(prompt("¿Cuanto quiere ingresar?"))
+        saldo = dinero + ingreso
+        document.write(`Usted ingresó ${ingreso}. Su saldo actual es ${saldo}`);
+        break;
+    case 4:
+        document.write('ingreso una opción invalida');
+        break;
+
+    default:
+        break;
+}
