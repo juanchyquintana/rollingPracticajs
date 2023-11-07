c1037eed0f77bcd4f8d7e646633ad967bc003fd8
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-11-07 11:06:36 -0300

    ejercicio tres terminado

--- js/tp-3/ejercicio-1.js
+++ js/tp-3/ejercicio-1.js
@@ -1,3 +1,7 @@
+// Buenas profe, cómo le va? Le quería avisar por este medio que los primeros cuatro ejercicios son los del panel de trello. Luego vienen los solicitados en el TP3 que publicaron en el Area Personal. H
+
+// Hago esta aclaración, ya que en el tp2 paso una situación similar y tuve unas observaciones a causa de eso. Desde ya muchas gracias, saludos.
+
 const numeroAlto = (numero) => {
     let numeroMayor = numero[0];
     for(let i = 1; i < numero.length; i++) {
